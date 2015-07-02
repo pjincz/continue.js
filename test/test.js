75ee3d61a47ac750ab043fbd4bbab5634eaf85bd
jinchizhong <jinchizhong@kingsoft.com>
2015-07-03 02:11:31 +0800

    unittest go through

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var S = require('../lib/continue.js');
+var C = require('../lib/continue.js');
 
 describe('self test', function () {
   it('1 == 1', function () {
@@ -20,41 +20,41 @@ var mock_func2 = function(r1, r2, cb) {
 };
 
 describe('continue.js', function () {
-  it('S(func)(func)(func)', function (done) {
-    S(function (c) {
+  it('C(func)(func)(func)', function (done) {
+    C(function (c) {
       c(123, 234);
-    })(function (c) {
-      assert.equal(c.value, 123);
-      assert.equal(c.args[1], 234);
+    })(function (c, locals) {
+      assert.equal(locals.args[0], 123);
+      assert.equal(locals.args[1], 234);
       c();
-    }).end(done);
+    }).done(done);
   });
-  it('S.then(func).then(func).then(func)', function (done) {
-    S.then(function (c) {
+  it('C.then(func).then(func).then(func)', function (done) {
+    C.then(function (c) {
       c(123, 234);
-    }).then(function (c) {
-      assert.equal(c.value, 123);
-      assert.equal(c.args[1], 234);
+    }).then(function (c, locals) {
+      assert.equal(locals.args[0], 123);
+      assert.equal(locals.args[1], 234);
       c();
-    }).end(done);
+    }).done(done);
   });
   it('call by apply', function () {
-    S(function (c) {
+    C(function (c) {
       c.apply(this, {});
     }).end();
   });
   it('pass value by attr', function (done) {
-    S(function (c) {
-      c.x = 123;
+    C(function (c, locals) {
+      locals.x = 123;
       c();
-    })(function (c) {
-      assert.equal(c.x, 123);
+    })(function (c, locals) {
+      assert.equal(locals.x, 123);
       c();
-    }).end(done);
+    }).done(done);
   });
   it('exception', function () {
     try {
-      S(function (c) {
+      C(function (c) {
         throw 'test';
       }).end();
     }
@@ -63,211 +63,201 @@ describe('continue.js', function () {
     }
   });
   it('next name conflict', function () {
-    S(function (c) {
+    C(function (c) {
       c.next = 123;
       c();
     }).end();
   });
   it('sync', function () {
-    S(function (c) {
-      c.x = 123;
+    C(function (c, locals) {
+      locals.x = 123;
       c();
-    })(function (c) {
-      assert.equal(c.x, 123);
+    })(function (c, locals) {
+      assert.equal(locals.x, 123);
       c();
     }).end();
   });
   it('async', function (done) {
-    S(function (c) {
+    C(function (c) {
       setTimeout(c, 10);
     })(function (c) {
       assert.equal(c.value, undefined);
       c();
-    }).end(done);
+    }).done(done);
   });
   it('exception', function (done) {
-    S(function (c) {
+    C(function (c) {
       throw 'test';
     })(function (c) {
       throw 'should not exec over';
-    }).err(function (c) {
-      assert.equal(c.err, 'test');
+    })(function (err, c, locals) {
+      assert.equal(err, 'test');
       c();
-    })(function (c) {
-      c.x = 123;
+    })(function (c, locals) {
+      locals.x = 123;
       c();
-    }).end(function(err, c) {
-      assert.equal(c.x, 123);
+    }).done(function(err, locals) {
+      assert(!err);
+      assert.equal(locals.x, 123);
       done();
     });
   });
-  it('exception pass to end', function (done) {
-    S(function (c) {
+  it('exception pass to done', function (done) {
+    C(function (c) {
       throw 'test';
     })(function (c) {
       throw 'should not exec over';
-    }).end(function (err) {
+    }).done(function (err) {
       assert.equal(err, 'test');
       done();
     });
   });
-  it('test report', function (done) {
-    var func = function(err, v) {
-      assert.equal(err, null);
+  it('test end assign', function (done) {
+    var func = function(v) {
       assert.equal(v, 'hello');
       done();
     };
-    S(function (c) {
-      c.x = 'hello';
+    C(function (c, locals) {
+      locals.x = 'hello';
       c();
-    }).report('x', func);
+    }).end('x', func);
   });
-  it('test report 2', function (done) {
-    var func = function(err, v) {
+  it('test end assign 2', function (done) {
+    var func = function(err) {
       assert.equal(err, 'an error');
-      assert.equal(v, null);
       done();
     };
-    S(function (c) {
+    C(function (c) {
       throw 'an error';
-    }).report('x', func);
+    }).end(true, 'err', func);
   });
   it('alias .err(err, c)', function (done) {
-    S(function (c) {
+    C(function (c) {
       throw 'test';
-    }).err(function (err, c) {
+    })(function (err, c, locals) {
       assert.equal(err, 'test');
-      assert.equal(c.err, 'test');
-      c();
-    }).end(done);
-  });
-  it('alias (err, c)', function (done) {
-    S(function (c) {
-      throw 'test';
-    })(function (err, c) {
-      assert.equal(err, 'test');
-      assert.equal(c.err, 'test');
-      c();
-    }).end(done);
-  });
-  it('alias done()', function (done) {
-    S(function (c) {
-      throw 'test';
-    })(function (err, c) {
-      assert.equal(err, 'test');
-      assert.equal(c.err, 'test');
+      assert.equal(locals.err, undefined);
       c();
     }).done(done);
   });
-  it('test assigner', function (done) {
-    S(function (c) {
-      mock_func2('test', 1234, c.assigner('x', 'y'));
-    })(function (c) {
-      assert.equal(c.x, 'test');
-      assert.equal(c.y, 1234);
+  it('alias (err, c)', function (done) {
+    C(function (c) {
+      throw 'test';
+    })(function (err, c, locals) {
+      assert.equal(err, 'test');
+      assert.equal(locals.err, undefined);
       c();
-    }).end(done);
+    }).done(done);
   });
-  it('test assigner2', function (done) {
+  it('test assign', function (done) {
+    C(function (c) {
+      mock_func2('test', 1234, c.assign('x', 'y'));
+    })(function (c, locals) {
+      assert.equal(locals.x, 'test');
+      assert.equal(locals.y, 1234);
+      c();
+    }).done(done);
+  });
+  it('test assign2', function (done) {
     var x = {};
     var y = {};
-    S(function (c) {
-      mock_func2('test', 1234, c.assigner2(x, 'x', y, 'y'));
+    C(function (c) {
+      mock_func2('test', 1234, c.assign2(x, 'x', y, 'y'));
     })(function (c) {
       assert.equal(x.x, 'test');
       assert.equal(y.y, 1234);
       c();
-    }).end(done);
+    }).done(done);
   });
-  it('change err by assigner', function (done) {
+  it('change err by assign', function (done) {
     var x = {};
-    S(function (c) {
-      mock_func2('error', 1234, c.assigner2(c, 'err', x, 'value'));
+    C(function (c, locals) {
+      mock_func2('error', 1234, c.assign2(locals, 'err', x, 'value'));
     })(function (c) {
       throw 'never over here';
-    })(function (err, c) {
-      assert.equal(c.err, 'error');
+    })(function (err, c, locals) {
+      assert.equal(err, 'error');
       assert.equal(x.value, 1234);
       c();
-    }).end(done);
+    }).done(done);
   });
-  it('assigner ignore null', function (done) {
-    S(function (c) {
-      mock_func(123, c.assigner(null));
+  it('assign ignore null', function (done) {
+    C(function (c) {
+      mock_func(123, c.assign(null));
     })(function (c) {
-      mock_func(123, c.assigner2(null, null));
-    }).end(done);
+      mock_func(123, c.assign2(null, null));
+    }).done(done);
   });
   it('parameter number simulate', function (done) {
-    S(function (c) {
-      assert.equal(c.assigner().length, 0);
-      assert.equal(c.assigner(null).length, 1);
-      assert.equal(c.assigner(null, null).length, 2);
-      assert.equal(c.assigner(null, null, null).length, 3);
-      assert.equal(c.assigner(null, null, null, null).length, 4);
-      assert.equal(c.assigner(null, null, null, null, null).length, 5);
-      assert.equal(c.assigner(null, null, null, null, null, null).length, 6);
-      assert.equal(c.assigner(null, null, null, null, null, null, null).length, 7);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null).length, 8);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null).length, 9);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null).length, 10);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null, null).length, 11);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null, null, null).length, 12);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null, null, null, null).length, 13);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 14);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 15);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 16);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 17);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 18);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 19);
-      assert.equal(c.assigner(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 20);
+    C(function (c) {
+      assert.equal(c.assign().length, 0);
+      assert.equal(c.assign(null).length, 1);
+      assert.equal(c.assign(null, null).length, 2);
+      assert.equal(c.assign(null, null, null).length, 3);
+      assert.equal(c.assign(null, null, null, null).length, 4);
+      assert.equal(c.assign(null, null, null, null, null).length, 5);
+      assert.equal(c.assign(null, null, null, null, null, null).length, 6);
+      assert.equal(c.assign(null, null, null, null, null, null, null).length, 7);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null).length, 8);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null).length, 9);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null).length, 10);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null, null).length, 11);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null, null, null).length, 12);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null, null, null, null).length, 13);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 14);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 15);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 16);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 17);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 18);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 19);
+      assert.equal(c.assign(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 20);
       c();
-    }).end(done);
+    }).done(done);
   });
   it('parameter number simulate 2', function (done) {
-    S(function (c) {
-      assert.equal(c.assigner2().length, 0);
-      assert.equal(c.assigner2(null, null).length, 1);
-      assert.equal(c.assigner2(null, null, null, null).length, 2);
-      assert.equal(c.assigner2(null, null, null, null, null, null).length, 3);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null).length, 4);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null).length, 5);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null).length, 6);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 7);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+    C(function (c) {
+      assert.equal(c.assign2().length, 0);
+      assert.equal(c.assign2(null, null).length, 1);
+      assert.equal(c.assign2(null, null, null, null).length, 2);
+      assert.equal(c.assign2(null, null, null, null, null, null).length, 3);
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null).length, 4);
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null).length, 5);
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null).length, 6);
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null).length, 7);
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null).length, 8);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null).length, 9);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null).length, 10);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null).length, 11);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null).length, 12);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null, null, null).length, 13);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null, null, null, null, null
         ).length, 14);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null).length, 15);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null).length, 16);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null).length, 17);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null).length, 18);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null).length, 19);
-      assert.equal(c.assigner2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
+      assert.equal(c.assign2(null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null, null, null, null, null
         , null, null, null, null, null, null, null, null, null, null, null, null).length, 20);
       c();
-    }).end(done);
+    }).done(done);
   });
 });
